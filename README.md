3471825cdeea31b4a656998d2a9edbd44b63b408
Jérémy La Scala <jeremy.lascala@epfl.ch>
2023-07-25 17:03:06 +0200

    refactor: use app key

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Graasp App: brainwriting
+
+[![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-%23FE5196?logo=conventionalcommits&logoColor=white)](https://conventionalcommits.org)
--- src/modules/main/BuilderView.tsx
+++ src/modules/main/BuilderView.tsx
@@ -1,7 +1,7 @@
 import { BUILDER_VIEW_CY } from '@/config/selectors';
 
 const BuilderView = (): JSX.Element => (
-  <div data-cy={BUILDER_VIEW_CY}>Player</div>
+  <div data-cy={BUILDER_VIEW_CY}>Builder</div>
 );
 
 export default BuilderView;
