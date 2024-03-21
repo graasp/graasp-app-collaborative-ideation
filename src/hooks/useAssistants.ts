658dbd2a87979f2dc64d1989fd861a5c8f5bff77
Jérémy La Scala <jeremy.lascala@epfl.ch>
2024-03-21 18:10:35 +0100

    fix: hook automatic response generation on next steps

--- src/hooks/useAssistants.ts
+++ src/hooks/useAssistants.ts
@@ -1,16 +1,21 @@
 import { ChatbotRole } from '@graasp/sdk';
 
-import { ChatbotResponseAppData } from '@/config/appDataTypes';
+import { ChatbotResponseAppData, ResponseAppData } from '@/config/appDataTypes';
 import {
   DEFAULT_CHATBOT_RESPONSE_APP_DATA,
   RESPONSE_MAXIMUM_LENGTH,
 } from '@/config/constants';
-import { getSingleResponsePrompt } from '@/config/prompts';
+import {
+  getSingleResponsePrompt,
+  promptForSingleResponse,
+  promptForSingleResponseAndProvideResponses,
+} from '@/config/prompts';
 import { mutations } from '@/config/queryClient';
 import { AssistantPersona } from '@/interfaces/assistant';
 import { useAppDataContext } from '@/modules/context/AppDataContext';
 import { useSettings } from '@/modules/context/SettingsContext';
 import { useTranslation } from 'react-i18next';
+import { useActivityContext } from '@/modules/context/ActivityContext';
 
 interface UseAssistantsValues {
   generateSingleResponse: () => Promise<ChatbotResponseAppData | undefined>;
@@ -24,13 +29,25 @@ interface UseAssistantsValues {
   reformulateResponse: (
     response: string,
   ) => Promise<ChatbotResponseAppData | undefined>;
+  generateResponsesWithEachAssistant: () => Promise<
+    Promise<ResponseAppData | undefined>[]
+  >;
 }
 
 const useAssistants = (): UseAssistantsValues => {
   const { t } = useTranslation('translations', { keyPrefix: 'PROMPTS' });
   const { mutateAsync: postChatBot } = mutations.usePostChatBot();
-  const { chatbot, instructions: generalPrompt, assistants } = useSettings();
+  const {
+    chatbot,
+    instructions: generalPrompt,
+    assistants,
+    instructions,
+  } = useSettings();
   const { postAppDataAsync } = useAppDataContext();
+
+  const { assistantsResponsesSets, round, allResponses, postResponse } =
+    useActivityContext();
+
   const reformulateResponse = (
     response: string,
   ): Promise<ChatbotResponseAppData | undefined> =>
@@ -114,11 +131,56 @@ const useAssistants = (): UseAssistantsValues => {
   ): Promise<Promise<ChatbotResponseAppData | undefined>[]> =>
     assistants.assistants.map((p) => promptAssistant(p, prompt));
 
+  const generateResponsesWithEachAssistant = async (): Promise<
+    Promise<ResponseAppData | undefined>[]
+  > => {
+    const responsesAssistants = assistants.assistants
+      .map((persona) => {
+        const assistantSet = assistantsResponsesSets.find(
+          (set) =>
+            set.data.assistant === persona.id && set.data.round === round - 1,
+        );
+        if (assistantSet) {
+          const responses = assistantSet.data.responses.map(
+            (r) => allResponses.find(({ id }) => r === id)?.data.response || '',
+          );
+          return promptAssistant(
+            persona,
+            promptForSingleResponseAndProvideResponses(
+              instructions.title.content,
+              responses,
+              t,
+            ),
+          );
+        }
+        return promptAssistant(
+          persona,
+          promptForSingleResponse(instructions.title.content, t),
+        );
+      })
+      .map((promise) =>
+        promise.then((assistantResponseAppData) => {
+          if (assistantResponseAppData) {
+            const response = assistantResponseAppData.data.completion;
+            return postResponse({
+              response,
+              round,
+              bot: true,
+            });
+          }
+          return assistantResponseAppData;
+        }),
+      );
+
+    return responsesAssistants;
+  };
+
   return {
     generateSingleResponse,
     promptAssistant,
     promptAllAssistants,
     reformulateResponse,
+    generateResponsesWithEachAssistant,
   };
 };
 
