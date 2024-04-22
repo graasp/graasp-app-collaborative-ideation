f03df35f2aa9c5d5e5dbe7038fc25587f67c6092
Jérémy La Scala <jeremy.lascala@epfl.ch>
2024-04-22 16:01:40 +0200

    feat: add instruction mode and enable use of instruction details

--- src/config/prompts.ts
+++ src/config/prompts.ts
@@ -1,6 +1,7 @@
 import { TFunction } from 'i18next';
 
-import { RESPONSE_MAXIMUM_LENGTH } from './constants';
+import { RESPONSE_MAXIMUM_LENGTH } from '@/config/constants';
+import { PromptMode } from '@/interfaces/assistant';
 
 export const DEFAULT_SYSTEM_PROMPT =
   'You are part of a group of designers trying to solve a wicked problem. Help your group by proposing new ideas';
@@ -13,23 +14,69 @@ export const getSingleResponsePrompt = (prompt: string): string =>
 export const promptForSingleResponse = (
   problemStatement: string,
   t: TFunction<'prompts', undefined>,
-): string =>
-  t('SINGLE_RESPONSE', {
-    problemStatement,
-    maxChars: RESPONSE_MAXIMUM_LENGTH,
-  });
+  details?: string,
+  promptMode?: PromptMode,
+): string => {
+  if (promptMode === PromptMode.Instructions) {
+    return typeof details === 'undefined'
+      ? t('INSTRUCTIONS.SINGLE_RESPONSE', {
+          instructions: problemStatement,
+          maxChars: RESPONSE_MAXIMUM_LENGTH,
+        })
+      : t('INSTRUCTIONS.SINGLE_RESPONSE_WITH_DETAILS', {
+          // TODO: Put this in translations
+          instructions: problemStatement,
+          details,
+          maxChars: RESPONSE_MAXIMUM_LENGTH,
+        });
+  }
+  return typeof details === 'undefined'
+    ? t('PROBLEM.SINGLE_RESPONSE', {
+        problemStatement,
+        maxChars: RESPONSE_MAXIMUM_LENGTH,
+      })
+    : t('PROBLEM.SINGLE_RESPONSE_WITH_DETAILS', {
+        // TODO: Put this in translations
+        problemStatement,
+        details,
+        maxChars: RESPONSE_MAXIMUM_LENGTH,
+      });
+};
 
 export const promptForSingleResponseAndProvideResponses = (
   problemStatement: string,
   responses: string[],
   t: TFunction<'prompts', undefined>,
+  details?: string,
+  promptMode?: PromptMode,
 ): string => {
   const responsesString = responses.map(
     (r) => `<solution>\n${r}\n</solution>\n`,
   );
-  return t('SINGLE_RESPONSE_AND_PROVIDE_RESPONSES', {
-    problemStatement,
-    responses: responsesString,
-    maxChars: RESPONSE_MAXIMUM_LENGTH,
-  });
+  if (promptMode === PromptMode.Instructions) {
+    return typeof details === 'undefined'
+      ? t('INSTRUCTIONS.SINGLE_RESPONSE_AND_PROVIDE_RESPONSES', {
+          instructions: problemStatement,
+          responses: responsesString,
+          maxChars: RESPONSE_MAXIMUM_LENGTH,
+        })
+      : t('INSTRUCTIONS.SINGLE_RESPONSE_WITH_DETAILS_AND_PROVIDE_RESPONSES', {
+          instructions: problemStatement,
+          details,
+          responses: responsesString,
+          maxChars: RESPONSE_MAXIMUM_LENGTH,
+        });
+  }
+  return typeof details === 'undefined'
+    ? t('PROBLEM.SINGLE_RESPONSE_AND_PROVIDE_RESPONSES', {
+        instructions: problemStatement,
+        responses: responsesString,
+        maxChars: RESPONSE_MAXIMUM_LENGTH,
+      })
+    : t('PROBLEM.SINGLE_RESPONSE_WITH_DETAILS_AND_PROVIDE_RESPONSES', {
+        instructions: problemStatement,
+        details,
+        responses: responsesString,
+        maxChars: RESPONSE_MAXIMUM_LENGTH,
+      });
 };
--- src/modules/settings/assistant/Assistant.tsx
+++ src/modules/settings/assistant/Assistant.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { ChangeEvent, FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import Button from '@mui/material/Button';
@@ -8,11 +8,22 @@ import cloneDeep from 'lodash.clonedeep';
 import { v4 as uuidv4 } from 'uuid';
 
 import { AssistantsSetting } from '@/config/appSettingsType';
-import { AssistantPersona, makeEmptyAssistant } from '@/interfaces/assistant';
+import {
+  AssistantPersona,
+  PromptMode,
+  makeEmptyAssistant,
+} from '@/interfaces/assistant';
 import SectionTitle from '@/modules/adminPanel/SectionTitle';
 
-import AssistantCard from './AssistantCard';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import AssistantDialog from './AssistantDialog';
+import AssistantCard from './AssistantCard';
 
 interface AssistantProps {
   assistants: AssistantsSetting;
@@ -27,7 +38,24 @@ const Assistant: FC<AssistantProps> = ({
     keyPrefix: 'SETTINGS.ASSISTANT',
   });
   const [editedAssistant, setEditedAssistant] = useState<AssistantPersona>();
-  const { assistants } = assistantsSetting;
+  const { assistants, includeDetails, promptMode } = assistantsSetting;
+
+  const handleIncludeDetailsChange = (
+    _e: ChangeEvent<HTMLInputElement>,
+    checked: boolean,
+  ): void => {
+    onChange({
+      ...assistantsSetting,
+      includeDetails: checked,
+    });
+  };
+
+  const handlePromptModeChange = (e: SelectChangeEvent): void => {
+    onChange({
+      ...assistantsSetting,
+      promptMode: e.target.value as PromptMode,
+    });
+  };
 
   const handleSave = (newAssistant: AssistantPersona): void => {
     const newAssistants = cloneDeep(assistants);
@@ -38,6 +66,7 @@ const Assistant: FC<AssistantProps> = ({
       newAssistants[index] = newAssistant;
     }
     onChange({
+      ...assistantsSetting,
       assistants: newAssistants,
     });
     setEditedAssistant(undefined);
@@ -58,6 +87,34 @@ const Assistant: FC<AssistantProps> = ({
   return (
     <>
       <SectionTitle>{t('TITLE')}</SectionTitle>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={includeDetails}
+              onChange={handleIncludeDetailsChange}
+            />
+          }
+          label={t('USE_DETAILS')}
+        />
+      </FormGroup>
+      <FormControl>
+        <InputLabel id="prompt-mode-label">{t('PROMPT_MODE_LABEL')}</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={promptMode}
+          label={t('PROMPT_MODE_LABEL')}
+          onChange={handlePromptModeChange}
+        >
+          <MenuItem value={PromptMode.Instructions}>
+            {t('PROMPT_MODE_ITEMS.INSTRUCTIONS')}
+          </MenuItem>
+          <MenuItem value={PromptMode.Problem}>
+            {t('PROMPT_MODE_ITEMS.PROBLEM')}
+          </MenuItem>
+        </Select>
+      </FormControl>
       <Grid container spacing={1}>
         {assistants.map((persona) => (
           <Grid key={persona.id} item>
