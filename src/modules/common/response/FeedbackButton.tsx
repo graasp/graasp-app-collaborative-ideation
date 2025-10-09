92d92b2b1a3cd55ef608fc604a4ebe939cdb4695
Jérémy La Scala <jeremy.lascala@epfl.ch>
2025-10-09 13:27:16 +0200

    fix: linear progress for feedback

--- src/modules/common/response/FeedbackButton.tsx
+++ src/modules/common/response/FeedbackButton.tsx
@@ -1,21 +1,49 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
+import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
+import LinearProgress from '@mui/material/LinearProgress';
 
+import { WAITING_TIME_FEEDBACK_GENERATION_S } from '@/config/constants';
 import useFeedback from '@/hooks/feedback/useFeedback';
 import { ResponseData } from '@/interfaces/response';
 import { Thread } from '@/interfaces/threads';
 import { useLoroContext } from '@/state/LoroContext';
 import { FEEDBACK_PROCESSING_IDS_KEY } from '@/state/TmpState';
 
+const LINEAR_PROGRESS_PERIOD_MS = 200;
+const LINEAR_PROGRESS_INCREMENT =
+  (100 * LINEAR_PROGRESS_PERIOD_MS) /
+  (1000 * WAITING_TIME_FEEDBACK_GENERATION_S);
+
 const FeedbackButton: FC<{ response: ResponseData; thread: Thread }> = ({
   response,
   thread,
 }) => {
+  const { t } = useTranslation('translations', { keyPrefix: 'RESPONSE_CARD' });
   const { generateFeedback } = useFeedback();
   const { tmpState } = useLoroContext();
 
   const [isBeingProcessed, setIsBeingProcessed] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [errorGettingFeedback, setErrorGettingFeedback] = useState(false);
+  const timer = useRef<NodeJS.Timeout>(null);
+
+  const startLinearProgress = (): void => {
+    timer.current = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress >= 100) {
+          if (timer.current) {
+            clearInterval(timer.current);
+          }
+          setErrorGettingFeedback(true);
+          return 100;
+        }
+        return oldProgress + LINEAR_PROGRESS_INCREMENT;
+      });
+    }, LINEAR_PROGRESS_PERIOD_MS);
+  };
 
   useEffect(() => {
     const unsubscribe = tmpState.subscribe(() => {
@@ -23,6 +51,7 @@ const FeedbackButton: FC<{ response: ResponseData; thread: Thread }> = ({
 
       if (Array.isArray(processingIds)) {
         setIsBeingProcessed(processingIds.includes(response.id));
+        startLinearProgress();
       }
     });
     return () => unsubscribe();
@@ -41,14 +70,21 @@ const FeedbackButton: FC<{ response: ResponseData; thread: Thread }> = ({
     }
   };
 
-  return (
+  return isBeingProcessed ? (
+    <>
+      <LinearProgress variant="determinate" value={progress} />
+      {errorGettingFeedback && (
+        <Alert severity="error">{t('ERROR_GETTING_FEEDBACK')}</Alert>
+      )}
+    </>
+  ) : (
     <Button
       size="small"
       onClick={handleFeedback}
       disabled={isBeingProcessed}
       loading={isBeingProcessed}
     >
-      Get Feedback
+      {t('GET_FEEDBACK')}
     </Button>
   );
 };
